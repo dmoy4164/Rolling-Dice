0bd390615d3340b96e8b7637158fc6fe31d0b727
dm <114600332+dmoy4164@users.noreply.github.com>
2023-09-29 15:48:25 -0400

    comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
+//declare variables
 const button = document.querySelector("button");
-button.addEventListener("click", roll);
 const aud = document.querySelector("audio");
 
+//add event listener to button
+button.addEventListener("click", roll);
+
+//function to roll the dice, print the new values, print the sum, and play audio
 function roll(){
     const num = Math.floor(Math.random()*6)+1;
     const num2 = Math.floor(Math.random()*6)+1;
